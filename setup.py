9cd1eddcb09493f4a128a82a9eb0036386f0b2e7
jgomezdans <gomezdansj@gmail.com>
2010-05-15 18:10:15 +0100

    Second commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='julespy',
+      version=version,
+      description="A python driver to run the JULES model.",
+      long_description="""\
+This code is a wrapper to the JULES land surface model. The main aims is to have easy access to parameters, drivers and model output from python.""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Jose Gomez-Dans',
+      author_email='j.gomez-dans@geog.ucl.ac.uk',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
